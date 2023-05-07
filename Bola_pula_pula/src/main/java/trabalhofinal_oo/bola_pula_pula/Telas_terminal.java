e4e0f0fdd2878277c33692145f05de871ea1b258
BrenoMontanha <breno.montanha@estudante.ufjf.br>
2023-05-07 12:42:58 -0300

    Preparando os Menus em Terminal

---
+++ Bola_pula_pula/src/main/java/trabalhofinal_oo/bola_pula_pula/Telas_terminal.java
@@ -0,0 +1,64 @@
+package trabalhofinal_oo.bola_pula_pula;
+
+import java.util.Scanner;
+
+public class Telas_terminal {
+
+    public static void Menu_Inicial(){
+        Scanner teclado =new Scanner(System.in);
+        int escolha;
+        do{
+            clean();
+            System.out.println("\t\t\t\tBola pula pula\n");
+            System.out.println("\t1-Jogar\n\t2-Ranking\n\t3-Regras\n\t4-Creditos\n\t5-sair");
+            escolha=teclado.nextInt();
+            teclado.nextLine();
+            switch(escolha){
+                case 1:
+                    //Tabuleiro Jogo= new Tabuleiro();
+                    break;
+                case 2:
+                    Ranking();
+                    break;
+                case 3:
+                    Regras();
+                    break;
+                case 4:
+                    Creditos();
+                    break;
+                case 5:
+                    break;
+                default:
+                    System.out.println("Valor inserido não corresponde as opções, tente outro");
+                    try{Thread.sleep(1000);}
+                    catch(InterruptedException e){Thread.currentThread().interrupt();}
+            }
+        }while(escolha!=5);
+        teclado.close();
+    }
+
+    public static void clean(){
+        System.out.println("\033[H\033[2J");
+    }
+
+    private static void Ranking(){
+        //TODO:Implementar a função para mostrar o Ranking que será armazenado externamente
+    }
+
+    private static void Regras(){
+        clean();
+        System.out.println("\t\t\t\tRegras\n");
+        System.out.println("\tVocê controla uma barra que pode andar lateralmente");
+        System.out.println("\tO objetivo é quebrar os blocos que são gerados de tempos em tempos no topo do tabuleiro");
+        System.out.println("\tVocê tem 3 vidas, que são reduzidas quando o tabuleiro não possui mais nenhuma bolinha");
+        System.out.println("A cada bloco quebrado, sua pontuação total será acrescida e ao final das 3 vidas, será inserida no Ranking");
+        System.out.println("\nPara voltar, presione o Enter");
+        try{System.in.read();}
+        catch(Exception e){}
+    }
+
+    private static void Creditos(){
+        //TODO: Implementar os Créditos
+    }
+
+}
