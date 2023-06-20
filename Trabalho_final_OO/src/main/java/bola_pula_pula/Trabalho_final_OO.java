c47f18ec7a18cc8a07eb3ec2cb6712bf474acde4
Lucas Henrique <107961106+Lucas-Henriquee@users.noreply.github.com>
2023-06-20 13:17:49 -0300

    Interface Bola Pula Pula

---
+++ Trabalho_final_OO/src/main/java/bola_pula_pula/Trabalho_final_OO.java
@@ -0,0 +1,18 @@
+
+package bola_pula_pula;
+
+import view.Tela;
+
+/**
+ *
+ * @author lucas_henrique
+ */
+public class Trabalho_final_OO {
+
+    public static void main(String[] args) {
+        
+        Tela chama = new Tela();
+        chama.inicializa();
+        
+    }
+}
