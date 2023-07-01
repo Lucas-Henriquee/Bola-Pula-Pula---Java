82e11e694a8408b33f5c6513b50c868972c332b2
Lucas_Henrique <lucashn003@hotmail.com>
2023-07-01 10:16:15 -0300

    Redimensionando - corrigindo pequenos erros

--- TrabalhoFinal/src/main/java/view/TelaCreditos.java
+++ TrabalhoFinal/src/main/java/view/TelaCreditos.java
@@ -7,7 +7,6 @@ import java.awt.Font;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -16,51 +15,51 @@ import javax.swing.JTextArea;
 
 public class TelaCreditos extends JPanel implements ActionListener {
 
-     public TelaCreditos() {
+    public TelaCreditos() {
 
-          Tela.visor.getContentPane().removeAll();
+        Tela.visor.getContentPane().removeAll();
 
-          setLayout(null);
-          setBackground(Color.cyan);
+        setLayout(null);
+        setBackground(Color.cyan);
 
-          JLabel jlCreditos = new JLabel("Créditos");
-          jlCreditos.setForeground(Color.black);
-          jlCreditos.setFont(new Font("segoe UI", 1, 65));
-          jlCreditos.setBounds(new Rectangle(350, 35, 750, 90));
+        JLabel jlCreditos = new JLabel("Créditos");
+        jlCreditos.setForeground(Color.black);
+        jlCreditos.setFont(new Font("segoe UI", 1, 65));
+        jlCreditos.setBounds(new Rectangle(320, 35, 750, 90));
 
-          JTextArea jtaCreditos = new JTextArea();
-          jtaCreditos.setEditable(false);
-          jtaCreditos.setBackground(Color.cyan);
-          jtaCreditos.setFont(new Font("segoe UI", 1, 32));
-          jtaCreditos.setBounds(new Rectangle(25, 172, 900, 335));
-          jtaCreditos.setText("\n              Este é um trabalho de Orientação a Objetos\n\n      "
-                    + "    relizado por Breno Fernandes, Breno Montanha,\n\n                  "
-                    + "  Lucas Henrique e Pedro Henrique.\n\n\n\n               "
-                    + "Esperamos que tenham gostado do jogo :)");
+        JTextArea jtaCreditos = new JTextArea();
+        jtaCreditos.setEditable(false);
+        jtaCreditos.setBackground(Color.cyan);
+        jtaCreditos.setFont(new Font("segoe UI", 1, 26));
+        jtaCreditos.setBounds(new Rectangle(5, 160, 900, 335));
+        jtaCreditos.setText("\n              Este é um trabalho de Orientação a Objetos\n\n      "
+                + "    relizado por Breno Fernandes, Breno Montanha,\n\n                  "
+                + "  Lucas Henrique e Pedro Henrique.\n\n\n\n               "
+                + "Esperamos que tenham gostado do jogo :)");
 
-          JButton jbVoltar = new JButton("Voltar");
-          jbVoltar.setFont(new Font("Segoe UI", 1, 35));
-          jbVoltar.setForeground(Color.black);
-          jbVoltar.setBackground(new Color(153, 153, 153));
-          jbVoltar.setCursor(new Cursor(Cursor.HAND_CURSOR));
-          jbVoltar.setBounds(new Rectangle(330, 560, 250, 70));
-          jbVoltar.addActionListener(this);
+        JButton jbVoltar = new JButton("Voltar");
+        jbVoltar.setFont(new Font("Segoe UI", 1, 35));
+        jbVoltar.setForeground(Color.black);
+        jbVoltar.setBackground(new Color(153, 153, 153));
+        jbVoltar.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        jbVoltar.setBounds(new Rectangle(330, 560, 250, 70));
+        jbVoltar.addActionListener(this);
 
-          add(jlCreditos);
-          add(jtaCreditos);
-          add(jbVoltar);
+        add(jlCreditos);
+        add(jtaCreditos);
+        add(jbVoltar);
 
-          Tela.visor.getContentPane().add(this, BorderLayout.CENTER);
-          Tela.visor.revalidate();
-          Tela.visor.repaint();
-          Tela.visor.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-     }
+        Tela.visor.getContentPane().add(this, BorderLayout.CENTER);
+        Tela.visor.revalidate();
+        Tela.visor.repaint();
+        Tela.visor.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 
-     @Override
-     public void actionPerformed(ActionEvent e) {
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
-          new TelaMenu();
+        new TelaMenu();
 
-     }
+    }
 
 }
--- TrabalhoFinal/src/main/java/view/TelaRegras.java
+++ TrabalhoFinal/src/main/java/view/TelaRegras.java
@@ -7,7 +7,6 @@ import java.awt.Font;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -16,52 +15,52 @@ import javax.swing.JTextArea;
 
 public class TelaRegras extends JPanel implements ActionListener {
 
-     public TelaRegras() {
+    public TelaRegras() {
 
-          Tela.visor.getContentPane().removeAll();
+        Tela.visor.getContentPane().removeAll();
 
-          setLayout(null);
-          setBackground(Color.cyan);
+        setLayout(null);
+        setBackground(Color.cyan);
 
-          JLabel jlRegras = new JLabel("Regras");
-          jlRegras.setForeground(Color.black);
-          jlRegras.setFont(new Font("segoe UI", 1, 65));
-          jlRegras.setBounds(new Rectangle(340, 30, 750, 90));
+        JLabel jlRegras = new JLabel("Regras");
+        jlRegras.setForeground(Color.black);
+        jlRegras.setFont(new Font("segoe UI", 1, 65));
+        jlRegras.setBounds(new Rectangle(330, 30, 750, 90));
 
-          JTextArea jtaRegras = new JTextArea();
-          jtaRegras.setEditable(false);
-          jtaRegras.setBackground(Color.cyan);
-          jtaRegras.setFont(new Font("segoe UI", 1, 25));
-          jtaRegras.setBounds(new Rectangle(35, 180, 900, 335));
-          jtaRegras.setText("             O jogador controla uma barra que se move lateralmente. "
-                    + "\n\nO objetivo é quebrar os blcos que são gerados de tempos em tempos.\n\n  "
-                    + "            O jogador possui 3 vidas ao longo de cada partida.\n\n    "
-                    + "  As vidas são perdidas quando o tabuleiro não possui mais bolas.\n\n"
-                    + "A cada bloco quebrado a pontuação será acrescida na pontuação total.");
+        JTextArea jtaRegras = new JTextArea();
+        jtaRegras.setEditable(false);
+        jtaRegras.setBackground(Color.cyan);
+        jtaRegras.setFont(new Font("segoe UI", 1, 22));
+        jtaRegras.setBounds(new Rectangle(5, 180, 900, 335));
+        jtaRegras.setText("             O jogador controla uma barra que se move lateralmente. "
+                + "\n\nO objetivo é quebrar os blcos que são gerados de tempos em tempos.\n\n  "
+                + "            O jogador possui 3 vidas ao longo de cada partida.\n\n    "
+                + "  As vidas são perdidas quando o tabuleiro não possui mais bolas.\n\n"
+                + "A cada bloco quebrado a pontuação será acrescida na pontuação total.");
 
-          JButton jbVoltar = new JButton("Voltar");
-          jbVoltar.setFont(new Font("Segoe UI", 1, 35));
-          jbVoltar.setForeground(Color.black);
-          jbVoltar.setBackground(new Color(153, 153, 153));
-          jbVoltar.setCursor(new Cursor(Cursor.HAND_CURSOR));
-          jbVoltar.setBounds(new Rectangle(325, 560, 250, 70));
-          jbVoltar.addActionListener(this);
+        JButton jbVoltar = new JButton("Voltar");
+        jbVoltar.setFont(new Font("Segoe UI", 1, 35));
+        jbVoltar.setForeground(Color.black);
+        jbVoltar.setBackground(new Color(153, 153, 153));
+        jbVoltar.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        jbVoltar.setBounds(new Rectangle(325, 560, 250, 70));
+        jbVoltar.addActionListener(this);
 
-          add(jlRegras);
-          add(jtaRegras);
-          add(jbVoltar);
+        add(jlRegras);
+        add(jtaRegras);
+        add(jbVoltar);
 
-          Tela.visor.getContentPane().add(this, BorderLayout.CENTER);
-          Tela.visor.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-          Tela.visor.revalidate();
-          Tela.visor.repaint();
+        Tela.visor.getContentPane().add(this, BorderLayout.CENTER);
+        Tela.visor.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Tela.visor.revalidate();
+        Tela.visor.repaint();
 
-     }
+    }
 
-     @Override
-     public void actionPerformed(ActionEvent e) {
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
-          new TelaMenu();
+        new TelaMenu();
 
-     }
+    }
 }
